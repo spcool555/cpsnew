156258c90ba052eee95a79ab8e163da8c951262c
spcool555 <spcool555@gmail.com>
2024-05-10 15:13:58 +0530

    first commit

--- src/components/footer.svelte
+++ src/components/footer.svelte
@@ -3,13 +3,7 @@
 </script>
 
 <!-- svelte-ignore a11y-missing-attribute -->
-<a role="button" on:click={() => ($aboutTab = !$aboutTab)}>About</a>
-<p>
-	Created by <a href="https://github.com/rynidja">rynidja</a> &copy; {new Date().getFullYear()}
-</p>
-<a role="button" href="https://github.com/rynidja/cps-tester" class="github">
-	<img src="./assets/images/GitHub-min.png" alt="github" width="32" height="32"/>
-</a>
+
 
 <style>
 	:global(footer) {
